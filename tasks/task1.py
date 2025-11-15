4d88d4dbe1553e5b3c2dd5a963877b4807f29d89
atem228-gif <atem.228@mail.ru>
2025-11-15 13:14:26 +0500

    k

--- tasks/task1.py
+++ tasks/task1.py
@@ -2,9 +2,10 @@
 
 def solve():
 # Ниже пишите решение задачи
-    
-
-    
+    S = int(input())
+    x = S // 6         
+    katya = 4 * x    
+    print(x,katya,x) 
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
     solve()
